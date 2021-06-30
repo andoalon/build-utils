14d47322a546304a73f13d355cedb7514341e805
andoalon <andoalon97@gmail.com>
2021-07-01 01:42:06 +0200

    Add more error checking to static-msvc-runtime.cmake

--- cmake/static-msvc-runtime.cmake
+++ cmake/static-msvc-runtime.cmake
@@ -7,12 +7,38 @@ macro(set_old_msvc_runtime_policy)
     # currently detect the statically linked runtime.
     # If needs to be called before the 'project' call
     # (https://cmake.org/cmake/help/v3.15/prop_tgt/MSVC_RUNTIME_LIBRARY.html)
-    cmake_policy(SET CMP0091 OLD)
+    
+    if (PROJECT_NAME)
+        message(FATAL_ERROR "set_old_msvc_runtime_policy() must be called BEFORE calling project()")
+    endif()
+
+    if (POLICY CMP0091)
+        cmake_policy(SET CMP0091 OLD) # Modifies the policy of the caller
+    endif()
 endmacro()
 
-macro(set_static_msvc_runtime)
+function(set_static_msvc_runtime)
+    if (NOT PROJECT_NAME)
+        message(FATAL_ERROR "set_static_msvc_runtime() must be called AFTER calling project()")
+    endif()
+
+    if (NOT PROJECT_NAME STREQUAL CMAKE_PROJECT_NAME)
+        message(WARNING "Setting MSVC runtime for project ${PROJECT_NAME} which is not the top-level one (${CMAKE_PROJECT_NAME})")
+    endif()
+
+    if (POLICY CMP0091)
+        cmake_policy(POP) # We want to check the policies from the caller, not from this file
+
+        cmake_policy(GET CMP0091 msvc_runtime_policy)
+        if (NOT msvc_runtime_policy STREQUAL "OLD")
+            message(FATAL_ERROR "CMP0091 not correctly set to \"OLD\"")
+        endif()
+
+        cmake_policy(PUSH) # Keep amount of PUSH/POP equal
+    endif()
+
     if (MSVC)
-        set(CompilerFlags
+        set(compiler_flags
                 CMAKE_CXX_FLAGS
                 CMAKE_CXX_FLAGS_DEBUG
                 CMAKE_CXX_FLAGS_RELEASE
@@ -24,11 +50,16 @@ macro(set_static_msvc_runtime)
                 CMAKE_C_FLAGS_RELWITHDEBINFO
                 CMAKE_C_FLAGS_MINSIZEREL
                 )
-        foreach(CompilerFlag ${CompilerFlags})
-            string(REPLACE "/MD" "/MT" ${CompilerFlag} "${${CompilerFlag}}")
+        foreach(compiler_flag ${compiler_flags})
+            string(REPLACE "/MD" "/MT" ${compiler_flag} "${${compiler_flag}}")
+            set(${compiler_flag} "${${compiler_flag}}" PARENT_SCOPE)
+
+            # Not really necessary but otherwise the change won't show in
+            # the cache which can be confusing
+            set(${compiler_flag} "${${compiler_flag}}" CACHE STRING "" FORCE)
         endforeach()
 
         # The new way of doing it would be this: (cmake 3.15, and conan would need to start detecting this)
         #set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
     endif()
-endmacro()
+endfunction()
